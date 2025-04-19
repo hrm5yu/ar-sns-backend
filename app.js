426b146e290324127e0a1fb95fe4d41d4db3a7d1
hrm5yu <syugoru@gmail.com>
2025-04-19 22:32:46 +0900

    update error display

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ document.getElementById("submitMessage").addEventListener("click", function () {
 
   // 名前とメッセージが入力されていない場合の処理
   if (!name || !message) {
+    // エラーメッセージを画面に表示
+    document.getElementById("errorMessage").style.display = 'block';
     return;  // 入力がない場合、処理を中止してカメラを更新させる
   }
 
@@ -14,4 +16,7 @@ document.getElementById("submitMessage").addEventListener("click", function () {
   // 送信後、フォームをクリア
   document.getElementById("nameInput").value = "";
   document.getElementById("messageInput").value = "";
+
+  // エラーメッセージを非表示
+  document.getElementById("errorMessage").style.display = 'none';
 });
